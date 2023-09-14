a2ef06696921ffda759d5333b76f657ba97e0b6b
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-14 00:02:50 -0300

    update

--- data/chronocross.tsx
+++ data/chronocross.tsx
@@ -26,7 +26,7 @@ export const chronocross = [
     titulo: "Final Bueno",
     aventura: [
       {
-        texto: "- vencer al Time Devourer con las magias en el siguiente orden: Tierra, Fuego, Bosque, Agua, Gravedad, Cielo, y Chrono Cross. Usa magias de curación de para que el Time Devourer no lo cuente ni interrumpa tu orden de bolas",
+        texto: "- Vence al 'Time Devourer' con las magias en el siguiente orden: Tierra, Fuego, Bosque, Agua, Gravedad, Cielo, y Chrono Cross. Usa magias de curación de para que el 'Time Devourer' no lo cuente ni interrumpa tu orden de las magias",
         img: "",
         importante: false,
       },
@@ -37,7 +37,7 @@ export const chronocross = [
     titulo: "Final Malo",
     aventura: [
       {
-        texto: "- Vencer al Time Devourer",
+        texto: "- Vencer al 'Time Devourer' ",
         img: "",
         importante: false,
       },
@@ -45,10 +45,109 @@ export const chronocross = [
   },
   {
     id: 1003,
-    titulo: "Final del programador",
+    titulo: "Final: Programmer's Ending",
     aventura: [
       {
-        texto: "- Vencer al Time Devourer",
+        texto: "- (ng+) Vencer al 'Time Devourer' inmediatamente sin reclutar compañeros",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1004,
+    titulo: "Final: General Kid",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' antes de infiltrate al 'Viper Manor'",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1005,
+    titulo: "Final: The True Hero",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' mientras 'Kid' siga envenenada",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1006,
+    titulo: "Final: The Magical Dreamers",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' antes de terminar 'Fort Dragonia'  con 'Nikki' y 'Razzly' en el equipo, y que 'Kid' se haya sanado",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1007,
+    titulo: "Final: New Beginnings",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' después de convertirte en 'Lynx'",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1008,
+    titulo: "Final: Darkened Fate",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' después de que 'Harle' deja del equipo",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1009,
+    titulo: "Final: Onward Dragoons",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' inmediatamente después de rescatar a 'Riddel'",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1010,
+    titulo: "Final: Career Change",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' inmediatamente después de vencer a 'Dario' y antes de recuperar el cuerpo de 'Serge'",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1011,
+    titulo: "Final: Return of the Downtrodden",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' inmediatamente después de que aparezca la 'Terra Tower'",
+        img: "",
+        importante: false,
+      },
+    ]
+  },
+  {
+    id: 1012,
+    titulo: "Final: Remaster Secret Ending",
+    aventura: [
+      {
+        texto: "- (ng+) Vencer al 'Time Devourer' y ve todos los créditos",
         img: "",
         importante: false,
       },
