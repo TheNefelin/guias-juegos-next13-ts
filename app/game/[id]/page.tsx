aeb8b8e1bef700db82e79eff23a39c4211a5271a
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-18 13:04:07 -0300

    update

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -12,20 +12,23 @@ const Game = async ({ params }: Params) => {
   const id = Number(params.id)
   const dt = new Controller()
   const data = await dt.get_game_byid(id)
+  //la funcion trae un arreglo filtrado por id por eso siempre sera data[0]
 
-  if (data.length === 0 ) return <></>
+  if (data.length === 0) return <></>
 
   return (
     <>
       <p className='indent-8 p-4 bg-base-200 shadow-md mb-4'>{data[0].text}</p>
-      <Image
-        className='m-auto shadow-xl mb-4'
-        src={data[0].imgBackground}
-        alt='background'
-        height={700}
-        width={700}
-      >
-      </Image>
+      {data[0].imgBackground.map(img => (
+        <Image
+          className='m-auto shadow-xl mb-4'
+          src={img}
+          alt='background'
+          height={700}
+          width={700}
+        >
+        </Image>
+      ))}
     </>
   )
 }
