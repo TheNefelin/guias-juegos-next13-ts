21a7347d2817dfbeb44b989a02385718c52e6d0e
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-18 15:19:51 -0300

    update

--- components/index/Pokemon.tsx
+++ components/index/Pokemon.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 interface Pokemon {
@@ -11,24 +12,35 @@ interface Pokemon {
   },
 }
 
-const Pokemon = async () => {
-  function randomID(min:number, max:number) {
+const Pokemon = () => {
+  const [newPokemon, setNewPokemon] = useState<Pokemon | null>(null)
+
+  const randomID = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomID(1, 1010)}`, { cache: 'no-cache' })
-  const pokemon: Pokemon = await response.json()
+  const getPokemon = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomID(1, 1010)}`, { cache: 'no-cache' })
+    const data: Pokemon = await response.json()
+    setNewPokemon(data)
+  }
+
+  useEffect(() => {
+    getPokemon()
+  }, [])
 
   return (
-    <button className='bg-base-100/10 rounded-full backdrop-blur-sm fixed z-10 p-0'>
-      <Image
-        className="m-0 p-0"
-        src={pokemon.sprites.front_default}
-        alt={pokemon.name}
-        width={70}
-        height={70}
-        priority={true}
-      ></Image>
+    <button onClick={getPokemon} className='bg-base-100/10 rounded-full backdrop-blur-sm fixed z-10 p-0'>
+      {newPokemon &&
+        <Image
+          className="m-0 p-0"
+          src={newPokemon.sprites.front_default}
+          alt={newPokemon.name}
+          width={70}
+          height={70}
+          priority={true}
+        ></Image>
+      }
     </button>
   )
 }
