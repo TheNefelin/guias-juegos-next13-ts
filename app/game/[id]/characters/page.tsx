a3c0fe8c28c03b092945b30645edf0aa9f993a74
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-17 22:07:54 -0300

    update

--- app/game/[id]/characters/page.tsx
+++ app/game/[id]/characters/page.tsx
@@ -2,7 +2,7 @@ import Controller from '@/services/Controller'
 import React from 'react'
 import Image from 'next/image'
 
-import img from '../public/ChronoCross/characters/serge.jpg'
+import img from '@/publc/ChronoCross/characters/serge.jpg'
 
 interface Params {
   params: { id: string }
@@ -17,6 +17,25 @@ const Characters = async ({ params }: Params) => {
 
   return (
     <div className='bg-base-200 mb-2 card card-side shadow-xl flex gap-4 flex-wrap justify-center p-4'>
+
+      {data.map(character => (
+        <div  className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              className='w-auto pt-4'
+              src={character.img}
+              alt={character.name}
+              height={50}
+              width={50}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{character.name}</h2>
+            <p>{character.text}</p>
+          </div>
+        </div>
+      ))}
+
       {data.map(character => (
         <div key={character.id} className="card card-side bg-base-100 shadow-xl max-w-xs">
           <figure>
