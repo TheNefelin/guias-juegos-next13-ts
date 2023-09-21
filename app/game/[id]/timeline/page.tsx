ae9f9e61175242e3bcefb34e417e9f00184b6df2
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-21 20:37:19 -0300

    update

--- app/game/[id]/timeline/page.tsx
+++ app/game/[id]/timeline/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import Controller from '@/services/controller'
 import Check from '@/components/Check'
