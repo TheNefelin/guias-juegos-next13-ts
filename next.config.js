23f5aae04f608f5fc91b760641e8e04a21f87915
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-29 13:16:34 -0300

    update

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'bsite.net',
         port: '',
-        pathname: '/**',
+        pathname: '/metalflap/**',
       },
     ],
   },
